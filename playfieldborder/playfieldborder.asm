3c98c95a6be7703d1a94d8a0a715847a118d37ea
Guy Harwood <guy@milkchip.com>
2020-10-01 10:21:26 +0100

    change to 2 squares

--- playfieldborder/playfieldborder.asm
+++ playfieldborder/playfieldborder.asm
@@ -78,10 +78,11 @@ StartFrame:
     REPEND
 
     ; Set the next 164 lines only with PF0 third bit enabled
-    ldx #%00100000
+    ldx #%01100000
     stx PF0
     ldx #0
     stx PF1
+    ldx #%10000000
     stx PF2
     REPEAT 164
        sta WSYNC
